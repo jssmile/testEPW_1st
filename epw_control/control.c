f6c3da1b7d226255551a3903d1de196c25935cba
jssmile <jssmile19921112@gmail.com>
2016-04-01 18:49:09 +0800

    Set the forward encoder value from 90 to 100

--- epw_control/control.c
+++ epw_control/control.c
@@ -11,47 +11,56 @@ extern Encoder_t ENCODER_R;
 extern uint32_t SpeedValue_left;
 extern uint32_t SpeedValue_right;
 uint32_t cmd_cnt = 0;
-int cnt[2];
 
 
-void getData(){
-	detachEXTI(EXTI_Line0 | EXTI_Line1 | EXTI_Line2 | EXTI_Line3);
-	
-	cnt[0] = ENCODER_L.count;
-	cnt[1] = ENCODER_R.count;
 
-	resetEncoder(&ENCODER_L);
-	resetEncoder(&ENCODER_R);
-
-	USART_puts(USART3, "le_en:");
-	USART_putd(USART3, cnt[0]);
-	USART_puts(USART3, " ri_en:");
-	USART_putd(USART3, cnt[1]);
-	USART_puts(USART3, "\r\n");
-
-	attachEXTI(EXTI_Line0 | EXTI_Line1 | EXTI_Line2 | EXTI_Line3);
-}
+uint32_t mvl, mvr;
 
 void test_forward(){
-	ctrlTimer = xTimerCreate("encoder Polling", (Period), pdTRUE, (void *) 1, forward);
-	xTimerStart(ctrlTimer, 0);
+	cmd_cnt = 50;
+
+	if(mvl && mvr){
+		SpeedValue_left = mvl;
+		SpeedValue_right = mvr;
+	}
+
+		ctrlTimer = xTimerCreate("forward control", (Period), pdTRUE, (void *) 1, forward);
+		xTimerStart(ctrlTimer, 0);
 }
 
 void forward(){
-	getData();
+	int cnt[2];
+	cnt[0] = getEncoderLeft();
+	cnt[1] = getEncoderRight();
 
-	if(cnt[0] || cnt[1]) ++cmd_cnt;
+	if(cmd_cnt){
+		/* start counting only if encoder get data(motor moving)
+		 * moving period = cmd_cnt * Period */
+		if(cnt[0] || cnt[1]) --cmd_cnt;
+		SpeedValue_left += (cnt[0] < 90)? 1: (cnt[0] > 100)? -1: 0;
+		SpeedValue_right += (cnt[1] < 90)? 1: (cnt[1] > 100)? -1: 0;
 
-	if(cnt[0] < 100) SpeedValue_left++;
-	else if(cnt[0] > 100) SpeedValue_left--;
-	
-	if(cnt[1] < 100) SpeedValue_right++;
-	else if(cnt[1] > 100) SpeedValue_right--;
-
-	mMove(SpeedValue_left, SpeedValue_right);
-	if(cmd_cnt > cmd_times){
-		mStop();
-		xTimerStop(ctrlTimer, 0);
-		cmd_cnt = 0;
+		mMove(SpeedValue_left, SpeedValue_right);
+		/* record the value for next forward command */
+		mvl = SpeedValue_left;
+		mvr = SpeedValue_right;
 	}
+	else{
+		mStop();
+		if(!(cnt[0] || cnt[1])){
+			xTimerDelete(ctrlTimer, 0);
+		}
+	}
+
+	USART_puts(USART3, "fl:");
+	USART_putd(USART3, SpeedValue_left);
+	USART_puts(USART3, " fr:");
+	USART_putd(USART3, SpeedValue_right);
+	USART_puts(USART3, "\r\nel:");
+	USART_putd(USART3, cnt[0]);
+	USART_puts(USART3, " rl:");
+	USART_putd(USART3, cnt[1]);
+	USART_puts(USART3, "\r\n");
+
+	recControlData(SpeedValue_left, SpeedValue_right, cnt[0], cnt[1]);
 }
