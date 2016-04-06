ed8aeaac90fb8ae883cafca7777fe96bc112dc3e
jssmile <jssmile19921112@gmail.com>
2016-04-06 22:36:23 +0800

    Change the function call

--- epw_lib/command.c
+++ epw_lib/command.c
@@ -16,8 +16,7 @@ uint32_t inc = 1;
 struct receive_cmd_list * receive_cmd_type;
 
 void receive_task(){
-	//int i, j;
-	//while(1){
+
 		if(Receive_String_Ready){
 
 			//forward step by step 
@@ -46,83 +45,92 @@ void receive_task(){
 
 			//forward
 			if(received_string[0] == 'f'){
-				SpeedValue_left = 140;
-				SpeedValue_right = 140;
-				mMove(SpeedValue_left,SpeedValue_right);
-				USART_puts(USART3, "left:");
-				USART_putd(USART3, SpeedValue_left);
-				USART_puts(USART3, " right:");
-				USART_putd(USART3, SpeedValue_right);
-				USART_puts(USART3, "\r\n");
+				forward();
 			}
 
 			//backward
 			else if(received_string[0] == 'b'){
-				SpeedValue_left = 105;
-				SpeedValue_right = 105;
-				mMove(SpeedValue_left, SpeedValue_right);
-				USART_puts(USART3, "left:");
-				USART_putd(USART3, SpeedValue_left);
-				USART_puts(USART3, " right:");
-				USART_putd(USART3, SpeedValue_right);
-				USART_puts(USART3, "\r\n");
+				backward();
 			}
 
 			//left
 			else if(received_string[0] == 'l'){
-				SpeedValue_left = 105;
-				SpeedValue_right = 140;
-				mMove(SpeedValue_left, SpeedValue_right);
-				USART_puts(USART3, "left");
-				USART_putd(USART3, SpeedValue_left);
-				USART_puts(USART3, " right");
-				USART_putd(USART3, SpeedValue_right);
-				USART_puts(USART3, "\r\n");
+				left();
 			}
 
 			//right
 			else if(received_string[0] == 'r'){
-				SpeedValue_left = 140;
-				SpeedValue_right = 105;
-				mMove(SpeedValue_left, SpeedValue_right);
-				USART_puts(USART3, "left");
-				USART_putd(USART3, SpeedValue_left);
-				USART_puts(USART3, " right");
-				USART_putd(USART3, SpeedValue_right);
-				USART_puts(USART3, "\r\n");
+				right();
 			}
 
 			//stop
 			else if(received_string[0] == 's'){
-				SpeedValue_left = 120;
-				SpeedValue_right = 120;
-				mMove(SpeedValue_left, SpeedValue_right);
-				USART_puts(USART3, "left");
-				USART_putd(USART3, SpeedValue_left);
-				USART_puts(USART3, " right");
-				USART_putd(USART3, SpeedValue_right);
-				USART_puts(USART3, "\r\n");
+				stop();
 			}
 
+			//get encoder
 			else if(received_string[0] == 'e'){
 				getEncoder();
 			}
-			
-			//}
-			/*Receive_String_Ready = 0;
-			for( i = 0 ; i< MAX_STRLEN ; i++){
-				received_string[i]= 0;*/
+
+			//test
+			else if(received_string[0] == 't'){
+				test_forward();
+			}
 		}
-	//}
 }
-/*
-void parseString(unsigned char *received_string){
-	char *word = strtok(received_string,' ');
-	receive_cmd_type->Identifier[0] = word;
-	if (word = 'c'){
-			word = strtok(NULL,' ');
-			//receive_cmd_type->DIR_cmd = word;
-			//word = strtok(NULL,' ');
-			//receive_cmd_type->pwm_value = word;
-	}
-}*/
+
+void forward(){
+	SpeedValue_left = 140;
+	SpeedValue_right = 140;
+	mMove(SpeedValue_left,SpeedValue_right);
+	USART_puts(USART3, "left:");
+	USART_putd(USART3, SpeedValue_left);
+	USART_puts(USART3, " right:");
+	USART_putd(USART3, SpeedValue_right);
+	USART_puts(USART3, "\r\n");
+}
+
+void backward(){
+	SpeedValue_left = 105;
+	SpeedValue_right = 105;
+	mMove(SpeedValue_left, SpeedValue_right);
+	USART_puts(USART3, "left:");
+	USART_putd(USART3, SpeedValue_left);
+	USART_puts(USART3, " right:");
+	USART_putd(USART3, SpeedValue_right);
+	USART_puts(USART3, "\r\n");
+}
+
+void left(){
+	SpeedValue_left = 105;
+	SpeedValue_right = 140;
+	mMove(SpeedValue_left, SpeedValue_right);
+	USART_puts(USART3, "left");
+	USART_putd(USART3, SpeedValue_left);
+	USART_puts(USART3, " right");
+	USART_putd(USART3, SpeedValue_right);
+	USART_puts(USART3, "\r\n");
+}
+
+void right(){
+	SpeedValue_left = 140;
+	SpeedValue_right = 105;
+	mMove(SpeedValue_left, SpeedValue_right);
+	USART_puts(USART3, "left");
+	USART_putd(USART3, SpeedValue_left);
+	USART_puts(USART3, " right");
+	USART_putd(USART3, SpeedValue_right);
+	USART_puts(USART3, "\r\n");
+}
+
+void stop(){
+	SpeedValue_left = 120;
+	SpeedValue_right = 120;
+	mMove(SpeedValue_left, SpeedValue_right);
+	USART_puts(USART3, "left");
+	USART_putd(USART3, SpeedValue_left);
+	USART_puts(USART3, " right");
+	USART_putd(USART3, SpeedValue_right);
+	USART_puts(USART3, "\r\n");
+}
