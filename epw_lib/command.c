306d25b1c6e7b5b8a0cdcdde0c6cdbad597bb29a
jssmile <jssmile19921112@gmail.com>
2016-06-27 09:59:34 +0800

    Edit the LA command for the android app.

--- epw_lib/command.c
+++ epw_lib/command.c
@@ -15,169 +15,83 @@ uint32_t inc = 1;
 
 struct receive_cmd_list * receive_cmd_type;
 
-void receive_task(){
+void receive_task()
+{
 
-		if(Receive_String_Ready){
+    if(Receive_String_Ready) {
 
-			//forward step by step 
-			if(received_string[0] == '+'){
-				SpeedValue_left += inc;
-				SpeedValue_right += inc;
-				mMove(SpeedValue_left,SpeedValue_right);
-				USART_puts(USART3, "left:");
-				USART_putd(USART3, SpeedValue_left);
-				USART_puts(USART3, " right:");
-				USART_putd(USART3, SpeedValue_right);
-				USART_puts(USART3, "\r\n");
-			}
+        //forward
+        if(received_string[0] == 'f') {
+            test_PID_forward();
+        }
 
-			//backward step by step
-			else if(received_string[0] == '-'){
-				SpeedValue_left -= inc;
-				SpeedValue_right -= inc;
-				mMove(SpeedValue_left, SpeedValue_right);
-				USART_puts(USART3, "left:");
-				USART_putd(USART3, SpeedValue_left);
-				USART_puts(USART3, " right:");
-				USART_putd(USART3, SpeedValue_right);
-				USART_puts(USART3, "\r\n");
-			}
+        //backward
+        else if(received_string[0] == 'b') {
+            test_PID_backward();
+        }
 
-			//forward
-			else if(received_string[0] == 'p'){
-				forward();
-			}
+        //left
+        else if(received_string[0] == 'l') {
+            test_PID_left();
+        }
 
-			//PID
-			else if(received_string[0] == 'f'){
-				test_PID_forward();
-			}
+        //right
+        else if(received_string[0] == 'r') {
+            test_PID_right();
+        }
 
-			//backward
-			else if(received_string[0] == 'b'){
-				test_PID_backward();
-			}
+        //stop
+        else if(received_string[0] == 's') {
+            SpeedValue_left = 120;
+            SpeedValue_right = 120;
+            mMove(SpeedValue_left, SpeedValue_right);
+            USART_puts(USART3, "left");
+            USART_putd(USART3, SpeedValue_left);
+            USART_puts(USART3, " right");
+            USART_putd(USART3, SpeedValue_right);
+            USART_puts(USART3, "\r\n");
+        }
 
-			//left
-			else if(received_string[0] == 'l'){
-				test_PID_left();
-			}
+        //Linear Acturator
+        else if(received_string[0] == 'n') {
+            USART_puts(USART3, "Actu_A_up");
+            set_linearActuator_A_cmd(LINEAR_ACTU_CW);
+            USART_puts(USART3, "\r\n");
+        }
 
-			//right
-			else if(received_string[0] == 'r'){
-				test_PID_right();
-			}
+        else if(received_string[0] == 'd') {
+            USART_puts(USART3, "Actu_A_down");
+            //set_linearActuator_A_cmd(LINEAR_ACTU_CCW);
+            USART_puts(USART3, "\r\n");
+            set_linearActuator_A_cmd(LINEAR_ACTU_CCW);
+        } else if(received_string[0] == 'a') {
+            USART_puts(USART3, "Actu_A_stop");
+            //set_linearActuator_A_cmd(LINEAR_ACTU_CCW);
+            USART_puts(USART3, "\r\n");
+            set_linearActuator_A_cmd(LINEAR_ACTU_STOP);
+        }
 
-			//stop
-			else if(received_string[0] == 's'){
-				stop();
-			}
+        //Due to the
+        else if(received_string[0] == 'u'){
+            	USART_puts(USART3, "Actu_B_up");
+            	set_linearActuator_B_cmd(LINEAR_ACTU_CW);
+            	USART_puts(USART3, "\r\n");
+        	}
 
-			//Linear Acturator
-			else if(received_string[0] == 'a'){
-			    	USART_puts(USART3, "Actu_A_up");
-			    	set_linearActuator_A_cmd(LINEAR_ACTU_CW);
-			    	USART_puts(USART3, "\r\n");
-			    }
 
-			else if(received_string[0] == 'n'){
-			    	USART_puts(USART3, "Actu_A_down");
-			    	//set_linearActuator_A_cmd(LINEAR_ACTU_CCW);
-			    	USART_puts(USART3, "\r\n");
-			    	set_linearActuator_A_cmd(LINEAR_ACTU_CCW);
-			    }
-			else if(received_string[0] == 'd'){
-			    	USART_puts(USART3, "Actu_A_stop");
-			    	//set_linearActuator_A_cmd(LINEAR_ACTU_CCW);
-			    	USART_puts(USART3, "\r\n");
-			    	set_linearActuator_A_cmd(LINEAR_ACTU_STOP);
-			    }
+        else if(received_string[0] == 'k'){
+            	USART_puts(USART3, "Actu_B_down");
+            	//set_linearActuator_B_cmd(LINEAR_ACTU_CCW);
+            	USART_puts(USART3, "\r\n");
+            	set_linearActuator_B_cmd(LINEAR_ACTU_CCW);
+        	}
 
-			else if(received_string[0] == 'u'){
-			    	USART_puts(USART3, "Actu_B_up");
-			    	set_linearActuator_B_cmd(LINEAR_ACTU_CW);
-			    	USART_puts(USART3, "\r\n");
-				}
+        else if(received_string[0] == 'w'){
+            	USART_puts(USART3, "Actu_B_stop");
+            	//set_linearActuator_B_cmd(LINEAR_ACTU_CCW);
+            	USART_puts(USART3, "\r\n");
+            	set_linearActuator_B_cmd(LINEAR_ACTU_STOP);
+        	}
 
-				
-			else if(received_string[0] == 'k'){
-			    	USART_puts(USART3, "Actu_B_down");
-			    	//set_linearActuator_B_cmd(LINEAR_ACTU_CCW);
-			    	USART_puts(USART3, "\r\n");
-			    	set_linearActuator_B_cmd(LINEAR_ACTU_CCW);
-				}
-
-			else if(received_string[0] == 'w'){
-			    	USART_puts(USART3, "Actu_B_stop");
-			    	//set_linearActuator_B_cmd(LINEAR_ACTU_CCW);
-			    	USART_puts(USART3, "\r\n");
-			    	set_linearActuator_B_cmd(LINEAR_ACTU_STOP);
-				}
-
-			//get encoder
-			else if(received_string[0] == 'e'){
-				getEncoder();
-			}
-
-			//test
-			else if(received_string[0] == 't'){
-				test_forward();
-			}
-
-		}
-}
-
-void forward(){
-	SpeedValue_left = 140;
-	SpeedValue_right = 140;
-	mMove(SpeedValue_left,SpeedValue_right);
-	USART_puts(USART3, "left:");
-	USART_putd(USART3, SpeedValue_left);
-	USART_puts(USART3, " right:");
-	USART_putd(USART3, SpeedValue_right);
-	USART_puts(USART3, "\r\n");
-}
-
-void backward(){
-	SpeedValue_left = 105;
-	SpeedValue_right = 105;
-	mMove(SpeedValue_left, SpeedValue_right);
-	USART_puts(USART3, "left:");
-	USART_putd(USART3, SpeedValue_left);
-	USART_puts(USART3, " right:");
-	USART_putd(USART3, SpeedValue_right);
-	USART_puts(USART3, "\r\n");
-}
-
-void left(){
-	SpeedValue_left = 105;
-	SpeedValue_right = 140;
-	mMove(SpeedValue_left, SpeedValue_right);
-	USART_puts(USART3, "left");
-	USART_putd(USART3, SpeedValue_left);
-	USART_puts(USART3, " right");
-	USART_putd(USART3, SpeedValue_right);
-	USART_puts(USART3, "\r\n");
-}
-
-void right(){
-	SpeedValue_left = 140;
-	SpeedValue_right = 105;
-	mMove(SpeedValue_left, SpeedValue_right);
-	USART_puts(USART3, "left");
-	USART_putd(USART3, SpeedValue_left);
-	USART_puts(USART3, " right");
-	USART_putd(USART3, SpeedValue_right);
-	USART_puts(USART3, "\r\n");
-}
-
-void stop(){
-	SpeedValue_left = 120;
-	SpeedValue_right = 120;
-	mMove(SpeedValue_left, SpeedValue_right);
-	USART_puts(USART3, "left");
-	USART_putd(USART3, SpeedValue_left);
-	USART_puts(USART3, " right");
-	USART_putd(USART3, SpeedValue_right);
-	USART_puts(USART3, "\r\n");
+    }
 }
--- epw_lib/command.h
+++ epw_lib/command.h
@@ -8,10 +8,10 @@ extern uint8_t Receive_String_Ready;
 
 extern void receive_task();
 
-static struct receive_cmd_list{
-	volatile unsigned char Identifier[3];
-	volatile unsigned char DIR_cmd;
-	volatile unsigned char pwm_value;
+static struct receive_cmd_list {
+    volatile unsigned char Identifier[3];
+    volatile unsigned char DIR_cmd;
+    volatile unsigned char pwm_value;
 };
 
 #endif /* __COMMAND_H__ */
